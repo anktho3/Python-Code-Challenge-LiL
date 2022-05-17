cd631590ac8d8cf0474ffc7891f9b81650136f33
Andrew Thomas <andrew.thomas@rackspace.com>
2022-05-17 14:03:03 -0500

    Working waiting_game solution

---
+++ chal_05_WaitingGame.py
@@ -0,0 +1,25 @@
+import time
+from random import randint
+
+def waiting_game():
+    selectedTime = randint(1, 5)
+    print("Your target time is " + str(selectedTime) + " seconds.")
+    input("---Press Enter to Begin---")
+    timeStarted = time.time_ns()
+    input("...Press Enter again after {selectedTime} seconds...")
+    timeEnded = time.time_ns()
+
+    timeWaited = (timeEnded - timeStarted) / 1000000000
+
+    if timeWaited < selectedTime:
+        print(f"Elapsed time: {timeWaited:.3f} seconds")
+        print(f"{selectedTime - timeWaited:.3f} too fast")
+    elif timeWaited > selectedTime:
+        print(f"Elapsed time: {timeWaited:.3f} seconds")
+        print(f"{timeWaited - selectedTime:.3f} too slow")
+    elif timeWaited == selectedTime:
+        print(f"Elapsed time: {timeWaited:.3f} seconds")
+        print("Exact!")
+
+waiting_game()
+
