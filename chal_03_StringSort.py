dcc9a057fe8780b927f9cdefdf4157f8836929ae
Andrew Thomas <andrew.thomas@rackspace.com>
2022-05-17 11:24:20 -0500

    StringSort inital version

--- chal_03_StringSort.py
+++ chal_03_StringSort.py
@@ -1,1 +1,5 @@
-def stringSort()
+def stringSort(inputString):
+    loadedString = [word for word in inputString.split()]
+    sortedString = loadedString
+    sortedString.sort(key=str.lower)
+    return sortedString
