1bce074217137330ef438d27d1f50442d255b65f
Andrew Thomas <andrew.thomas@rackspace.com>
2022-05-18 14:08:35 -0500

    .gitignore added and updated chal 07

--- chal_07_Alarm.py
+++ chal_07_Alarm.py
@@ -1,3 +1,22 @@
-import time
+import time, sched
 import simpleaudio as sa
+
+
+
+def alarm(setTime, alarmSound, message):
+    wave_audio = sa.WaveObject.from_wave_file(alarmSound)
+    play_obj = wave_audio.play()
+
+    alarmTime = time.time() + float(setTime)
+    print("The alarm will ring in ", alarmTime, " seconds")
+
+    while time.time() != alarmTime:
+        print("sleeping")
+    if time.time() >= alarmTime:
+        for play_obj in range(5):
+            wave_audio.play()
+            print(play_obj)
+    
+    print(message)
+
 
