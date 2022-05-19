2bee2077002d36a7639114139d46a0003ea34df1
Andrew Thomas <andrew.thomas@rackspace.com>
2022-05-19 12:17:16 -0500

    Formatted chal_08_email reducing line count to under 30

--- chal_08_Email.py
+++ chal_08_Email.py
@@ -1,24 +1,19 @@
-import base64
+import base64, json, smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-import json
-import smtplib
 
 #Loading username and password for account from a separate secrets file
 f = open('config.secret',)
 data = json.load(f)
-
 
 def sendEmail(emailToaddr, subjectLine, messageBody):
     '''
     Given a receiver email address, subject line, and message body; this
     function will send an email to the address provided.
     '''
- 
     #setup the MIME
     message = MIMEMultipart()
-    #Decoding secrets as they are stored as base64 
-    message['From'] = str(base64.standard_b64decode(data["user_name"])).strip("b'")
+    message['From'] = str(base64.standard_b64decode(data["user_name"])).strip("b'")  #Decoding secrets as they are stored as base64
     message['To'] = str(emailToaddr)
     message['Subject'] = str(subjectLine)
 
@@ -26,8 +21,8 @@ def sendEmail(emailToaddr, subjectLine, messageBody):
     message.attach(MIMEText(messageBody, 'plain'))
 
     #Create STMP session for sending email
-    session = smtplib.SMTP('smtp.gmail.com', 587) #Using gmail with SMTP port
-    session.starttls() #enable secure connection
+    session = smtplib.SMTP('smtp.gmail.com', 587)  #Using gmail with SMTP port
+    session.starttls()  #enable secure connection
     session.login(str(base64.standard_b64decode(data["user_name"])).strip("b'"), str(base64.standard_b64decode(data["user_pass"])).strip("b'"))
     text = message.as_string()
     session.sendmail(str(base64.standard_b64decode(data["user_name"])).strip("b'"), emailToaddr, text)
